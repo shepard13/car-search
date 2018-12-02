17380321ad001942dbfd8a0136b37fbab39cb59f
Andrii Yurets <yurets.andrey13@gmail.com>
2018-12-02 15:35:12 +0200

    feat: use paste httpserver

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
-from bottle import run, get, error, request, template
+from bottle import run, get, error, request, template, default_app
+from paste import httpserver
 
+application = default_app()
 
 def get_file_content():
     with open('data.json', 'r') as f:
@@ -84,4 +86,4 @@ def get_cars():
 #
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=80, reloader=True, debug=True)
+    httpserver.serve(application, host='0.0.0.0', port=80)
